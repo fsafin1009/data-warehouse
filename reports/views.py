de1e948620c7ecbf7219a6825de512f7a2d148d5
Farukh Safin <webexpert1986@gmail.com>
2020-08-24 13:59:12 +0400

    24 AUG 13:31

--- reports/views.py
+++ reports/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Report, Category
 
 # Create your views here.
 
 
 def index(request):
-    return render(request, template_name='reports/index.html')
+    reports = Report.objects.all()
+    context = {
+        'reports' : reports,
+        'title': 'Список статей'
+    }
+    return render(request, template_name='reports/index.html', context = context)
 
 
